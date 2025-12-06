1430ab89cbf034100092cd1326b7e8323357680c
Wilfredo Sulbaran <wilfredo.sulbaran@epayco.com>
2025-12-06 15:36:50 -0500

    Remove CRUD methods from ContactsService in preparation for refactoring. Added package-lock.json to manage dependencies.

--- src/contacts/contacts.service.ts
+++ src/contacts/contacts.service.ts
@@ -11,33 +11,6 @@ export class ContactsService {
     @InjectRepository(Contact)
     private contactRepository: Repository<Contact>,
   ) {}
-
-  async create(createContactDto: CreateContactDto): Promise<Contact> {
-    const contact = this.contactRepository.create(createContactDto);
-    return await this.contactRepository.save(contact);
-  }
-
-  async findAll(): Promise<Contact[]> {
-    return await this.contactRepository.find();
-  }
-
-  async findOne(id: number): Promise<Contact> {
-    const contact = await this.contactRepository.findOne({ where: { id } });
-    if (!contact) {
-      throw new NotFoundException(`Contact with ID ${id} not found`);
-    }
-    return contact;
-  }
-
-  async update(id: number, updateContactDto: UpdateContactDto): Promise<Contact> {
-    const contact = await this.findOne(id);
-    Object.assign(contact, updateContactDto);
-    return await this.contactRepository.save(contact);
-  }
-
-  async remove(id: number): Promise<void> {
-    const contact = await this.findOne(id);
-    await this.contactRepository.remove(contact);
-  }
+  
 }
 
