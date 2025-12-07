be5ed726e6921482532c8d471c1024753e83087c
Wilfredo Sulbaran <wilfredo.sulbaran@epayco.com>
2025-12-07 14:20:39 -0500

    Add Log Analytics Workspace and Container Apps Environment resources
    - Introduced a Log Analytics Workspace for monitoring Container Apps.
    - Created a Container Apps Environment linked to the Log Analytics Workspace.
    - Added outputs for the Container Apps Environment name and ID, as well as the Log Analytics Workspace ID.

--- infraestructure/main.tf
+++ infraestructure/main.tf
@@ -8,3 +8,30 @@ resource "azurerm_resource_group" "main" {
     app         = var.app_name
   }
 }
+
+# Log Analytics Workspace (required for Container Apps)
+resource "azurerm_log_analytics_workspace" "main" {
+  name                = "${var.app_name}-logs"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+
+  tags = {
+    environment = "production"
+    app         = var.app_name
+  }
+}
+
+# Container Apps Environment
+resource "azurerm_container_app_environment" "main" {
+  name                       = "${var.app_name}-env"
+  location                   = azurerm_resource_group.main.location
+  resource_group_name        = azurerm_resource_group.main.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.main.id
+
+  tags = {
+    environment = "production"
+    app         = var.app_name
+  }
+}
